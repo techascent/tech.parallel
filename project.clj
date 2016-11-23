282a859aec3112da218e737cdebd822abe9277fc
Chris Nuernberger <chris@thinktopic.com>
2016-11-23 15:15:51 -0700

    Manually adding profiles.clj.enc

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.395"]])
+                 [org.clojure/core.async "0.2.395"]]
+
+  :think/meta {:type :library :tags [:low-level]})
