26e4a7b33f82ef92981a00c742a54416303433d4
Chris Nuernberger <chris@thinktopic.com>
2016-11-27 13:52:49 -0700

    Moving think.parallel back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.parallel "0.2.0"
+(defproject thinktopic/think.parallel "0.3.0-SNAPSHOT"
   :description "Library for parallelization primitives"
   :url "http://github.com/thinktopic/think.parallel"
   :license {:name "Eclipse Public License"
