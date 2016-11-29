42e22a4c6d0c1434e9ad2001026d7b2a4e2109fb
Chris Nuernberger <chris@thinktopic.com>
2016-11-29 11:36:11 -0700

    Bumping bugfix back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.parallel "0.3.4"
+(defproject thinktopic/think.parallel "0.3.5-SNAPSHOT"
   :description "Library for parallelization primitives"
   :url "http://github.com/thinktopic/think.parallel"
   :license {:name "Eclipse Public License"
