b7e534693fe54dff812e537b002aec434435f167
Chris Nuernberger <chris@thinktopic.com>
2017-04-25 13:37:46 -0600

    Release 0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.parallel "0.3.6-SNAPSHOT"
+(defproject thinktopic/think.parallel "0.3.6"
   :description "Library for parallelization primitives"
   :url "http://github.com/thinktopic/think.parallel"
   :license {:name "Eclipse Public License"
