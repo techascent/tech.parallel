4e43b8b2c418c12b89154ac09a5336c2993a78eb
Chris Nuernberger <chris@thinktopic.com>
2017-06-12 11:47:05 -0600

    Added claypool dependencies.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.441"]]
+                 [org.clojure/core.async "0.3.441"]
+                 [com.climate/claypoole "1.1.4"]]
 
   :think/meta {:type :library :tags [:low-level]})
