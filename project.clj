7c45c116ab0520026f992b0c6c6e34d8964e8b21
Chris Nuernberger <chris@thinktopic.com>
2017-12-22 17:47:19 -0700

    Release 0.3.8

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject thinktopic/think.parallel "0.3.8-SNAPSHOT"
+(defproject thinktopic/think.parallel "0.3.8"
   :description "Library for parallelization primitives"
   :url "http://github.com/thinktopic/think.parallel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.443"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.3.465"]]
 
   :think/meta {:type :library :tags [:low-level]})
