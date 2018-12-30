b7bbb4256e7c13682b22d4da2492151d49ace576
Chris Nuernberger <chris@thinktopic.com>
2018-12-30 15:08:44 -0700

    moved to deps.edn

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/tech-ascent/tech.parallel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.490"]])
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
