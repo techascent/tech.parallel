6daa0e2a8173b3c0a128b430a6843569a5a7ca00
Chris Nuernberger <chris@techascent.com>
2019-11-11 12:31:41 -0700

    2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.parallel "2.2-SNAPSHOT"
+(defproject techascent/tech.parallel "2.2"
   :description "Library for parallelization primitives"
   :url "http://github.com/tech-ascent/tech.parallel"
   :license {:name "Eclipse Public License"
