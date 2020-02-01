4d476146b52788a004c96eb6840d5231d77cac14
Chris Nuernberger <chris@techascent.com>
2020-02-01 10:59:09 -0700

    2.4

--- src/tech/parallel/utils.clj
+++ src/tech/parallel/utils.clj
@@ -8,20 +8,19 @@
      ~@(->> symbol-list
             (mapv
              (fn [sym-name]
-               `(do
-                  (let [varval# (requiring-resolve (symbol ~(name src-ns)
-                                                           ~(name sym-name)))
-                        var-meta# (meta varval#)]
-                    (when (:macro var-meta#)
-                      (throw
-                       (ex-info
-                        (format "Cannot export macros as this breaks aot: %s"
-                                '~sym-name)
-                        {:symbol '~sym-name})))
-                    (def ~(symbol (name sym-name)) @varval#)
-                    (alter-meta! #'~(symbol (name sym-name))
-                                 merge
-                                 (select-keys var-meta#
-                                              [:file :line :column
-                                               :doc
-                                               :column :tag])))))))))
+               `(let [varval# (requiring-resolve (symbol ~(name src-ns)
+                                                         ~(name sym-name)))
+                      var-meta# (meta varval#)]
+                  (when (:macro var-meta#)
+                    (throw
+                     (ex-info
+                      (format "Cannot export macros as this breaks aot: %s"
+                              '~sym-name)
+                      {:symbol '~sym-name})))
+                  (def ~(symbol (name sym-name)) @varval#)
+                  (alter-meta! #'~(symbol (name sym-name))
+                               merge
+                               (select-keys var-meta#
+                                            [:file :line :column
+                                             :doc
+                                             :column :tag]))))))))
